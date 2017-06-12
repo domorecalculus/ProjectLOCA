dabe2d105804b5e1d6905f9ad971224717434d6b
domorecalculus <yesac63@gmail.com>
2017-06-12 14:22:02 -0500

    Testing db on remote host instead of local

--- app.js
+++ app.js
@@ -2,27 +2,64 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 const fs = require('fs');
+var pg = require('pg');
 
 var app = express();
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 5000));
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-var markerData = JSON.parse(fs.readFileSync(__dirname + '/markers.json', 'utf8'));
+var markerData = {"markers":[]}; //Pre-database = JSON.parse(fs.readFileSync(__dirname + '/markers.json', 'utf8'));
 console.log(markerData);
 
 wss.on('connection', function(ws){
     console.log("A user connected");
+    pg.defaults.ssl = true;
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+      if (err) throw err;
+      console.log('Connected to postgres! Getting markers.');
+
+      client
+        .query('SELECT Name,Lat,Lng FROM Markers;')
+        .on('row', function(row) {
+          console.log(JSON.stringify(row));
+        });
+    });
     ws.send(JSON.stringify(markerData));
 
     ws.on('message', function(message){
         console.log(message);
         data = JSON.parse(message);
         if(data.type === 'add'){
-            markerData.markers.push({'name': data.name, 'latlng': data.latlng})
+            //Pre-database
+            //markerData.markers.push({'name': data.name, 'latlng': data.latlng})
+
+            pg.defaults.ssl = true;
+            pg.connect(process.env.DATABASE_URL, function(err, client) {
+              if (err) throw err;
+              console.log('Connected to postgres! Getting markers.');
+
+              client
+                .query('SELECT Name,Lat,Lng FROM Markers;')
+                .on('row', function(row) {
+                  console.log(JSON.stringify(row));
+                });
+            });
         } else if (data.type === 'delete') {
-            markerData.markers.splice(data.index, 1);
-            console.log("Marker deleted.");
+            //Pre-database
+            //markerData.markers.splice(data.index, 1);
+
+            pg.defaults.ssl = true;
+            pg.connect(process.env.DATABASE_URL, function(err, client) {
+              if (err) throw err;
+              console.log('Connected to postgres! Getting schemas...');
+
+              client
+                .query('SELECT table_schema,table_name FROM information_schema.tables;')
+                .on('row', function(row) {
+                  console.log(JSON.stringify(row));
+                });
+            });
         }
     });
 });
